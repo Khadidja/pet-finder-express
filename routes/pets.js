727ea5b6e34d469500c904e460b68a7a0886d929
Khadidja <khadidja.abdel@gmail.com>
2017-07-06 18:20:50 -0500

    Improved pets index UI and added show page with pet info

--- routes/pets.js
+++ routes/pets.js
@@ -1,5 +1,5 @@
 var express = require('express'),
-    router = express.Router(),
+    router = express.Router({ mergeParams: true }),
     request = require("request");
 
 /* GET pets index page. */
@@ -11,14 +11,38 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
     var location = req.body.location;
     var key = process.env.PET_FINDER_API_KEY;
-    var url = "http://api.petfinder.com/pet.find?key=" + key + "&location=" + location + "&format=json&count=5";
+    var url = "http://api.petfinder.com/pet.find?key=" + key + "&location=" + location + "&format=json&count=20";
 
     request(url, function(err, response, body) {
         if (err) {
             res.send(err);
         } else {
             var pets = parsePetsResponse(JSON.parse(body).petfinder);
-            res.render('./pets/index', { title: 'Pet Adoption', pets: pets });
+            res.render('./pets/index', { title: 'Pet Adoption', pets: pets, customStylesheet: "pets.css" });
+        }
+    });
+});
+
+router.get("/:id", function(req, res, next) {
+    var key = process.env.PET_FINDER_API_KEY;
+    var url = "http://api.petfinder.com/pet.get?key=" + key + "&id=" + req.params.id + "&format=json";
+
+    request(url, function(err, response, body) {
+        if (err) {
+            res.send(err);
+        } else {
+            var petfinderObj = JSON.parse(body).petfinder;
+            var pet = {
+                id: petfinderObj.pet.id.$t,
+                animal: petfinderObj.pet.animal.$t,
+                name: petfinderObj.pet.name.$t,
+                sex: (petfinderObj.pet.sex.$t == "M") ? "Male" : "Female",
+                age: petfinderObj.pet.age.$t,
+                size: petSize(petfinderObj.pet.size.$t),
+                profilePicture: petfinderObj.pet.media.photos.photo.find(isImageSizeX).$t,
+                description: petfinderObj.pet.description.$t
+            };
+            res.render('./pets/show', { title: 'Pet Adoption', pet: pet });
         }
     });
 });
@@ -28,17 +52,23 @@ function parsePetsResponse(petfinderObj) {
     var pets = [];
     if (petsCount == 1) {
         pets.push({
+            id: petfinderObj.pets.pet.id.$t,
             animal: petfinderObj.pets.pet.animal.$t,
             name: petfinderObj.pets.pet.name.$t,
-            sex: petfinderObj.pets.pet.sex.$t,
+            sex: (petfinderObj.pets.pet.sex.$t == "M") ? "Male" : "Female",
+            age: petfinderObj.pets.pet.age.$t,
+            size: petSize(petfinderObj.pets.pet.size.$t),
             profilePicture: petfinderObj.pets.pet.media.photos.photo.find(isImageSizeX).$t
         });
     } else {
         petfinderObj.pets.pet.forEach(function(pet) {
             pets.push({
+                id: pet.id.$t,
                 animal: pet.animal.$t,
                 name: pet.name.$t,
-                sex: pet.sex.$t,
+                sex: (pet.sex.$t == "M") ? "Male" : "Female",
+                age: pet.age.$t,
+                size: petSize(pet.size.$t),
                 profilePicture: pet.media.photos.photo.find(isImageSizePn).$t
             });
         }, this);
@@ -52,6 +82,17 @@ function isImageSizeX(element) {
 
 function isImageSizePn(element) {
     return element["@size"] == "pn";
+}
+
+function petSize(size) {
+    if (size == "L")
+        return "Large";
+    else if (size == "M")
+        return "Medium";
+    else if (size == "S")
+        return "Small";
+    else
+        return size;
 }
 
 module.exports = router;
