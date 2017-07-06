b6ca0cf1824379b23e981f7c9931935a25fb0d44
Khadidja <khadidja.abdel@gmail.com>
2017-07-05 20:37:09 -0500

    Parsing JSON response

--- routes/pets.js
+++ routes/pets.js
@@ -11,17 +11,47 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
     var location = req.body.location;
     var key = process.env.PET_FINDER_API_KEY;
-    var url = "http://api.petfinder.com/pet.find?key=" + key + "&location=" + location + "&format=json&count=2";
+    var url = "http://api.petfinder.com/pet.find?key=" + key + "&location=" + location + "&format=json&count=5";
 
     request(url, function(err, response, body) {
         if (err) {
             res.send(err);
         } else {
-            var pets = JSON.parse(body).petfinder.pets.pet;
+            var pets = parsePetsResponse(JSON.parse(body).petfinder);
             res.render('./pets/index', { title: 'Pet Adoption', pets: pets });
         }
     });
 });
 
+function parsePetsResponse(petfinderObj) {
+    var petsCount = petfinderObj.lastOffset.$t;
+    var pets = [];
+    if (petsCount == 1) {
+        pets.push({
+            animal: petfinderObj.pets.pet.animal.$t,
+            name: petfinderObj.pets.pet.name.$t,
+            sex: petfinderObj.pets.pet.sex.$t,
+            profilePicture: petfinderObj.pets.pet.media.photos.photo.find(isImageSizeX).$t
+        });
+    } else {
+        petfinderObj.pets.pet.forEach(function(pet) {
+            pets.push({
+                animal: pet.animal.$t,
+                name: pet.name.$t,
+                sex: pet.sex.$t,
+                profilePicture: pet.media.photos.photo.find(isImageSizePn).$t
+            });
+        }, this);
+    }
+    return pets;
+}
+
+function isImageSizeX(element) {
+    return element["@size"] == "x";
+}
+
+function isImageSizePn(element) {
+    return element["@size"] == "pn";
+}
 
 module.exports = router;
